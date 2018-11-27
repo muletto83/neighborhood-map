cdf7cc3d87d2eba1daaf024cb0a92180647c59ce
muletto83 <muletto83@users.noreply.github.com>
2018-11-26 22:38:59 -0500

    Google Map Loading Optimized

--- src/App.js
+++ src/App.js
@@ -1,48 +1,41 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { load_google_maps } from "./Avail";
+import MapComp from "./components/MapComp";
 
 class App extends Component {
   /* Initialization that requires DOM nodes should go here.
   *  If we need to load data from a remote endpoint,
   *  this is a good place to instantiate the network request. */
   componentDidMount() {
-    this.mapLoader()
-  }
+    let googleMapPromise = load_google_maps();
 
-  /*Function to load Google Map  */
-  mapLoader = () => {
-    scriptLoader("https://maps.googleapis.com/maps/api/js?key=AIzaSyAGwWhii5M0m32NbNxLY2EEyZZWKBfqdKA&callback=initMap");
-    window.initMap = this.initMap;
-  }
-  /*the body of the callback function that we find at the end of the URL for the googleapis
-  * for now with only thetwo required options for every map: center and zoom
-  * https://developers.google.com/maps/documentation/javascript/tutorial */
-  initMap = () => {
-    let map = new window.google.maps.Map(document.getElementById('map'), {
-      center: {lat: 34.1015, lng: -84.5194},
-      zoom: 15
+    Promise.all([googleMapPromise]).then(values => {
+      //Checking the the value content to evaluate our next move
+      console.log(values);
+      let google = values[0];
+      this.map = new google.maps.Map(document.getElementById("map"), {
+				// Center map on DFW Metroplex
+				center: {
+					lat: 34.101318,
+					lng: -84.519379
+				},
+				zoom: 14
+			});
     })
   }
 
+
   render() {
     return (
-      <main className="App">
+      <div className="App">
         {/*https://stackoverflow.com/questions/50503677/proper-use-of-aria-role-for-google-maps*/}
-        <div id="map" role="application" aria-label="Map Application"></div>
+        <MapComp />
         <img src={logo} className="App-logo" alt="logo in motion" />
-      </main>
+      </div>
     )
   }
 }
-/*creating a function to manage the laoding of script withing React
-* https://www.fullstackreact.com/articles/Declaratively_loading_JS_libraries/index.html*/
-function scriptLoader(url) {
-  let index = window.document.getElementsByTagName("script")[0];
-  let tag = window.document.createElement("script");
-  tag.src = url;
-  tag.async = true;
-  tag.defer = true;
-  index.parentNode.insertBefore(tag, index)
-}
+
 export default App;
---
+++ src/components/MapComp.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class MapComp extends Component {
+  render() {
+		return (
+			<main>
+				<div id="map" role="application" aria-label="Map Application" />
+			</main>
+		);
+	}
+}
