8e6664f98e0420de6b99c150f841263d0a3bee95
muletto83 <muletto83@users.noreply.github.com>
2018-11-27 11:13:48 -0500

    Creating Markers

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import MapComp from "./components/MapComp";
 
 class App extends Component {
   /* Initialization that requires DOM nodes should go here.
-  *  If we need to load data from a remote endpoint,
-  *  this is a good place to instantiate the network request. */
+  ** If we need to load data from a remote endpoint,
+  ** this is a good place to instantiate the network request. */
   componentDidMount() {
     let gMapPromise = load_google_maps();
     let restaurantsPromise = loadRestaurants();
@@ -16,13 +16,33 @@ class App extends Component {
       //Checking the the value content to evaluate our next move
       console.log(values);
       let google = values[0];
+      let restaurants = values[1];
+
+      this.google = google;
+      this.markers = [];
+
       this.map = new google.maps.Map(document.getElementById("map"), {
 				// Coordinate of Woodstock GA
 				center: {
 					lat: 34.101318,
 					lng: -84.519379
 				},
-				zoom: 14
+				zoom: 13
+			});
+
+      /* For each restaurant we will have a marker on the map
+      ** The animation is a DROP from the sky :)
+      ** https://developers.google.com/maps/documentation/javascript/markers */
+      restaurants.forEach(restaurant => {
+				let marker = new google.maps.Marker({
+					position: { lat: restaurant.position.lat, lng: restaurant.position.lng },
+					map: this.map,
+					id: restaurant.id,
+					name: restaurant.name,
+					address: restaurant.address,
+					website: restaurant.website,
+					animation: google.maps.Animation.DROP
+				});
 			});
     })
   }
